7bcc6e33ed88e5667700fff5d909905941f11fec
OpiumBird <145978078+MarcosMartinsCarvalho@users.noreply.github.com>
2025-01-16 11:30:13 +0100

    help
    help

--- Cuphead Vertical slice/Assets/scripts/PlayerMovement.cs
+++ Cuphead Vertical slice/Assets/scripts/PlayerMovement.cs
@@ -16,13 +16,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private KeyCode moveRight = KeyCode.D;
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
     [SerializeField] private KeyCode dashKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode shootKey = KeyCode.F;
 
     private int lastDirection = 0;
     private bool isDashing = false;
     private PlayerState currentState = PlayerState.Idle;
     private Animator animator;
 
-    private enum PlayerState { Idle, MovingLeft, MovingRight, Jumping, Dashing }
+    private enum PlayerState
+    {
+        Idle,
+        MovingLeft,
+        MovingRight,
+        Jumping,
+        Dashing,
+        Shooting,
+        RunningAndShooting
+    }
 
     void Start()
     {
@@ -47,7 +57,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleInput()
     {
-        if (Input.GetKey(moveLeft) && !Input.GetKey(moveRight))
+        bool isMoving = Input.GetKey(moveLeft) || Input.GetKey(moveRight);
+        bool isShooting = Input.GetKey(shootKey);
+
+        if (isMoving && isShooting)
+        {
+            currentState = PlayerState.RunningAndShooting;
+        }
+        else if (Input.GetKey(moveLeft) && !Input.GetKey(moveRight))
         {
             currentState = PlayerState.MovingLeft;
             lastDirection = -1;
@@ -56,6 +73,10 @@ public class PlayerMovement : MonoBehaviour
         {
             currentState = PlayerState.MovingRight;
             lastDirection = 1;
+        }
+        else if (isShooting)
+        {
+            currentState = PlayerState.Shooting;
         }
         else if (isGrounded && !isDashing)
         {
@@ -82,20 +103,29 @@ public class PlayerMovement : MonoBehaviour
         switch (currentState)
         {
             case PlayerState.MovingLeft:
+            case PlayerState.RunningAndShooting:
                 rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
                 break;
+
             case PlayerState.MovingRight:
                 rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
                 break;
+
             case PlayerState.Jumping:
                 rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
                 isGrounded = false;
                 currentState = PlayerState.Idle;
                 break;
+
             case PlayerState.Dashing:
                 StartCoroutine(PerformDash());
                 break;
+
             case PlayerState.Idle:
+                rb.velocity = new Vector2(0, rb.velocity.y);
+                break;
+
+            case PlayerState.Shooting:
                 rb.velocity = new Vector2(0, rb.velocity.y);
                 break;
         }
@@ -124,6 +154,8 @@ public class PlayerMovement : MonoBehaviour
         animator.SetBool("IsWalking", currentState == PlayerState.MovingLeft || currentState == PlayerState.MovingRight);
         animator.SetBool("IsJumping", currentState == PlayerState.Jumping);
         animator.SetBool("IsDashing", currentState == PlayerState.Dashing);
+        animator.SetBool("IsShooting", currentState == PlayerState.Shooting);
+        animator.SetBool("IsRunningAndShooting", currentState == PlayerState.RunningAndShooting);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
